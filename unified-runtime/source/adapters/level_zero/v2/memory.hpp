c5fdb974894209287cb5db7052e834525537e9fb
Agata Momot <agata.momot@intel.com>
2025-06-22 18:54:56 +0000

    comment out unnecessary header in memory.hpp

--- unified-runtime/source/adapters/level_zero/v2/memory.hpp
+++ unified-runtime/source/adapters/level_zero/v2/memory.hpp
@@ -19,7 +19,7 @@
 #include "../image_common.hpp"
 #include "command_list_manager.hpp"
 #include "common.hpp"
-#include "lockable.hpp"
+// #include "lockable.hpp"
 
 using usm_unique_ptr_t = std::unique_ptr<void, std::function<void(void *)>>;
 
