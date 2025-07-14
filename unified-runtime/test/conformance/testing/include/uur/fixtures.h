4d1736f8dfcfbdef47844ab44920c0eced22ae8d
Agata Momot <agata.momot@intel.com>
2025-07-14 11:24:46 +0000

    bacl to normal

--- unified-runtime/test/conformance/testing/include/uur/fixtures.h
+++ unified-runtime/test/conformance/testing/include/uur/fixtures.h
@@ -406,8 +406,8 @@ struct urQueueTest : urContextTest {
 
   ur_queue_properties_t queue_properties = {UR_STRUCTURE_TYPE_QUEUE_PROPERTIES,
                                             nullptr, 
-                                            // 0};
-                                            UR_QUEUE_FLAG_SUBMISSION_BATCHED}; //0};
+                                            0};
+                                            // UR_QUEUE_FLAG_SUBMISSION_BATCHED}; //0};
   ur_queue_handle_t queue = nullptr;
 };
 
@@ -479,8 +479,8 @@ template <class T> struct urQueueTestWithParam : urContextTestWithParam<T> {
   }
   ur_queue_properties_t queue_properties = {UR_STRUCTURE_TYPE_QUEUE_PROPERTIES,
                                             nullptr, 
-                                            // 0};
-                                            UR_QUEUE_FLAG_SUBMISSION_BATCHED};
+                                            0};
+                                            // UR_QUEUE_FLAG_SUBMISSION_BATCHED};
   ur_queue_handle_t queue = nullptr;
 };
 
