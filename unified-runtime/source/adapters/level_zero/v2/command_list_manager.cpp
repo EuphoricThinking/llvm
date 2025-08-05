17495efed43fb3b59b5d482f750ad0504452edcf
Agata Momot <agata.momot@intel.com>
2025-08-05 19:51:45 +0000

    added logic on command_list_manager side; at least compiles; going to add runbatch

--- unified-runtime/source/adapters/level_zero/v2/command_list_manager.cpp
+++ unified-runtime/source/adapters/level_zero/v2/command_list_manager.cpp
@@ -503,8 +503,8 @@ ur_result_t ur_command_list_manager::appendTimestampRecordingExp(
 
 ur_result_t ur_command_list_manager::appendRegular(ze_command_list_handle_t *phCommandLists) {
    ZE2UR_CALL(zeCommandListImmediateAppendCommandListsExp,
-             getZeCommandList(), 1, phCommandLists,
-              nullptr, 0, wait_list_view{nullptr, 0});
+             (getZeCommandList(), 1, phCommandLists,
+              nullptr, 0, nullptr));
 
   return UR_RESULT_SUCCESS;
 }
