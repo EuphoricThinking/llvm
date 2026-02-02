792877dd53702c60b20cc68cdb06c9cea69d45de
Agata Momot <agata.momot@intel.com>
2026-02-02 14:53:01 +0000

    add sycl e2e tests for batched queues in L0v1 and L0v2

--- sycl/test-e2e/ProfilingTag/in_order_queue.cpp
+++ sycl/test-e2e/ProfilingTag/in_order_queue.cpp
@@ -1,6 +1,7 @@
 // REQUIRES: aspect-ext_oneapi_queue_profiling_tag
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
+// RUN: %if level_zero %{ env SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=0 UR_L0_V2_FORCE_BATCHED=1 %{run} %t.out %}
 
 // Tests the get_profiling_tag extension function on an in-order queue.
 
